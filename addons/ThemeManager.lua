54f5585cad11e290a083008671befaf959acabeb
angercc <87264662+angercc@users.noreply.github.com>
2023-03-19 13:00:18 +0000

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -12,10 +12,9 @@ local ThemeManager = {} do
 		['Tokyo Night'] 	= { 5, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"323232"}') },
 		['Ubuntu'] 			= { 6, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"3e3e3e","AccentColor":"e2581e","BackgroundColor":"323232","OutlineColor":"191919"}') },
 		             ['Monochrome']                   = { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1b1a1a","AccentColor":"ffffff","BackgroundColor":"1c1c1c","OutlineColor":"3c3c3c"}') },
-		['Tauper']  = { 8, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1d1810","AccentColor":"0c0c0c","BackgroundColor":"141414","OutlineColor":"120f0f"}') },
-	        ['Abyss'] = {9, httpService:JSONDecode('{"OutlineColor":"0a0a0a","Accent":"8c87b4","FontColor":"ffffff","MainColor":"1e1e1e","BackgroundColor":"141414"}') },
-['Fatality'] = {10, httpService:JSONDecode('{"OutlineColor":"0f0f28","Accent":"f00f50","FontColor":"c8c8ff","MainColor":"231946","BackgroundColor":"191432""}') },
-['Neverlose'] = {11, httpService:JSONDecode('{"OutlineColor":"000005","Accent":"00b4f0","FontColor":"ffffff","MainColor":"000f1e","BackgroundColor":"050514"}') },
+		
+['Fatality'] = {8, httpService:JSONDecode('{"OutlineColor":"0f0f28","Accent":"f00f50","FontColor":"c8c8ff","MainColor":"231946","BackgroundColor":"191432""}') },
+['Neverlose'] = {9, httpService:JSONDecode('{"OutlineColor":"000005","Accent":"00b4f0","FontColor":"ffffff","MainColor":"000f1e","BackgroundColor":"050514"}') },
 
 		
 	}
